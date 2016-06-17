bbf36178174186cb4da6fbf8c870e0981973c6cd
bradyz <brady.zhou@utexas.edu>
2016-06-16 20:46:05 -0400

    shell script

--- install.sh
+++ install.sh
@@ -1,32 +1,26 @@
 clear
 
+fun () {
+  BACKUP=$1
+  TYPE=$2
+  if [ -$TYPE "$HOME/$BACKUP" ]; then
+      COUNT=1
+      BACKUP_NAME="$HOME/$BACKUP$COUNT.bak"
+
+      while [ -f "$BACKUP_NAME" ]; do 
+          let COUNT=COUNT+1
+          BACKUP_NAME="$BACKUP_backup$COUNT"
+      done
+
+      mv $HOME/$BACKUP $BACKUP_NAME
+      echo "$HOME/$BACKUP has been backed up to $BACKUP_NAME"
+  fi
+}
+
 echo "Installing your brand new vim"
 
-if [ -f ~/.vimrc ]; then
-    COUNT=1
-    VIMRC_BACKUP_NAME="$HOME/.vimrc_backup$COUNT"
-
-    while [ -f "$VIMRC_BACKUP_NAME" ]; do 
-        let COUNT=COUNT+1
-        VIMRC_BACKUP_NAME="$HOME/.vimrc_backup$COUNT"
-    done
-
-    mv ~/.vimrc $VIMRC_BACKUP_NAME
-    echo "Your old vimrc has been moved to $VIMRC_BACKUP_NAME"
-fi
-
-if [ -d ~/.vim ]; then
-    COUNT=1
-    VIM_DIR_BACKUP_NAME="$HOME/.vim_backup$COUNT"
-
-    while [ -f "$VIM_DIR_BACKUP_NAME" ]; do 
-        let COUNT=COUNT+1
-        VIMRC_BACKUP_NAME="$HOME/.vim_backup$COUNT"
-    done
-
-    mv ~/.vim $VIM_DIR_BACKUP_NAME
-    echo "Your old vimrc has been moved to $VIM_DIR_BACKUP_NAME"
-fi
+fun ".vimrc"
+fun ".vim"
 
 git clone https://github.com/bradyz/dot-files.git ~/.vim/
 cd ~/.vim/
